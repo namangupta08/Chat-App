b772e5be865ce4adfc91ad62326caa74c7ca88ef
namangupta08 <guptanaman032@gmail.com>
2024-08-04 22:45:30 +0530

    middleware added

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,19 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/authRoutes.js"
 import connectDB from "./config/database.js";
 
+const PORT = process.env.PORT || 4000
 const app = express();
+
+
 dotenv.config();
 
-const PORT = process.env.PORT || 4000
-
-
 app.use('/api/auth' , authRoutes)
+app.use(express.json())
 
 connectDB();
 
-app.get("/" , (req,res  ) => {
-    res.send("hello world!!!")
-})
+// app.get("/" , (req,res  ) => {
+//     res.send("hello world!!!")
+// })
 
 app.listen(PORT , () => console.log(`Server running on port ${PORT}`));
