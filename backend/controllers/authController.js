084ab368ee4b774ce0baddf4fc14757816755e72
namangupta08 <guptanaman032@gmail.com>
2024-08-04 22:45:11 +0530

    auth controller updated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,34 @@
+import User from "../models/U ser.js";
 
 export const signup = async (res,req) => {
     try {
         
+        const {fullName , userName , password , confrimPassword , gender} = req.body;
+
+        if(password != confrimPassword){
+            return res.status(400).json({
+                success: false,
+                message:"Password and Confirm Password do not match. Please try again.",
+            });
+        }
+        
+        const user = await User.findOne({userName});
+
+        if(user){
+            return res.status(400).json({
+                success: false,
+                message:"User Already exists",
+            });
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     } catch (error) {
         
     }
