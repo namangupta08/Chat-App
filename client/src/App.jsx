ed5c9254b4f6b4978ac57929a02684bd71a88da6
namangupta08 <guptanaman032@gmail.com>
2024-08-10 08:25:20 +0530

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Button } from './components/ui/button'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/auth' element={<Auth/>}></Route>
+        <Route path='/chat' element={<Chat/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+        <Route path='*' element={<Navigate to= '/auth'/>}></Route>
+      </Routes>
     </div>
   )
 }
