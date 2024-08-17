9757982977545aa9f19bfceb7d0176bfdcdcf655
namangupta08 <guptanaman032@gmail.com>
2024-08-17 20:46:35 +0530

    Server initiated

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,9 +1,28 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import React, { useState } from 'react'
 import Background from '@/assets/login2.png'
 import Victory from '@/assets/victory.svg'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Input } from "@/components/ui/input"
+
+
 
 function Auth() {
+
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("")
+
+  const handleLogin = async () => {
+
+  }
+
+  const handleSignup = async () => {
+
+  }
+
+
+
   return (
     <div className='h-[100vh] w-[100vw] flex justify-center items-center'>
         <div className='h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2'>
@@ -21,8 +40,69 @@ function Auth() {
             </div>
 
             <div className='flex items-center justify-center w-full'>
+             <Tabs className='w-3/4'>
+                <TabsList className='bg-transparent w-full rounded-none'>
+                  <TabsTrigger value="login"
+                  className='data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300'
+                  >Login</TabsTrigger>
+                  <TabsTrigger 
+                  className='data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300'
+                  value="signup">SignUp</TabsTrigger>
+                </TabsList>
 
+                <TabsContent className='flex flex-col gap-5 mt-10' value="login">
+                  <Input 
+                  value={email}
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  onChange={(e) => setEmail(e.target.value)}
+                  />
+
+                  <Input 
+                  value={password}
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
+
+                <Button className='rounded-full p-6' onclick={handleLogin}>Login</Button>
+
+                </TabsContent>
+                <TabsContent className='flex flex-col gap-5' value="signup">
+                  <Input 
+                    value={email}
+                    placeholder="Email"
+                    type="email"
+                    className="rounded-full p-6"
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <Input 
+                    value={password}
+                    placeholder="Password"
+                    type="password"
+                    className="rounded-full p-6"
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <Input 
+                    value={confirmPassword}
+                    placeholder="Confirm Password"
+                    type="password"
+                    className="rounded-full p-6"
+                    onChange={(e) => setconfirmPassword(e.target.value)}
+                    />
+
+                    <Button className='rounded-full p-6' onclick={handleSignup}>Sign Up</Button>
+                </TabsContent>
+             </Tabs>
             </div>
+          </div>
+
+          <div className='hidden xl:flex justify-center items-center'>
+            <img src={Background} alt='background' className='h-[700px]'/>
           </div>
             
           
