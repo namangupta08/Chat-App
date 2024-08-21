bc0b294a44936fb7160e55e0b90df617ed611c8f
namangupta08 <guptanaman032@gmail.com>
2024-08-21 22:53:55 +0530

    updation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
       <App />
+      <Toaster closeButton/>
   </BrowserRouter>
     
   
