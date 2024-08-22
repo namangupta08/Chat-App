930f81a74a692d9598e21fd7e8ba15ad508ac5b8
namangupta08 <guptanaman032@gmail.com>
2024-08-22 23:07:24 +0530

    controller updated

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,8 +1,43 @@
+import User from "../models/UserModel.js";
+import jwt  from "jsonwebtoken";
 
+
+const maxAge = 3*24*60*60*1000;
+const createToken = (email , id) => {
+    return jwt.sign({email , id} , process.env.JWT_KEY , {expiresIn:maxAge})
+};
 
 export const SignUp = async (req,res , next) => {
     try {
         const {email , password} = req.body;
+        if(!email || !password){
+            return res.status(400).send({
+                success: false,
+                message: "Email and password are required",
+            })
+        }
+
+        const user = await User.create({email , password});
+
+        //jwt token
+
+        res.cookie("JWT" , createToken(email , user.id) , {
+            maxAge,
+            secure:true,
+            sameSite:"None"
+        });
+
+        return res.status(201).json({
+            user:{
+                id:user.id,
+                email:user.email,
+                // firstName:user.firstName,
+                // lastName:user.lastName,
+                // image:user.image,
+                profileSetup:user.profileSetup
+            }
+        })
+
     } 
     catch (error) {
         console.log(error);
