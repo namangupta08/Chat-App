3fd61dc3f33901587da6ca04d81e4f30014083ac
namangupta08 <guptanaman032@gmail.com>
2024-08-26 23:41:25 +0530

    updation in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,77 @@
-import React from 'react'
-import { Button } from './components/ui/button'
+
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Auth from './pages/auth'
 import Chat from './pages/chat'
 import Profile from './pages/profile'
+import { Children, useEffect, useState } from 'react'
+import { useAppStore } from './store'
+import { ChromeIcon } from 'lucide-react'
+import { apiClient } from './lib/api-client'
+import { GET_USER_INFO } from './utils/constants'
 
 function App() {
+
+  const {userInfo , setUserInfo} = useAppStore();
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+         const response = await apiClient.get(GET_USER_INFO , {withCredentials:true})
+         console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    if(!userInfo){
+      getUserData();
+    }else{
+      setLoading(false);
+    }
+
+  } , [userInfo , setUserInfo])
+
+  if(loading){
+    return <div>loading...</div>
+  }
+
+  const PrivateRoute = ({children}) => {
+    const {userInfo} = useAppStore();
+    const isAuthenticated = !!userInfo
+    return isAuthenticated ? children : <Navigate to='/auth'/>
+  }
+
+  const AuthRoute = ({children}) => {
+    const {userInfo} = useAppStore();
+    const isAuthenticated = !!userInfo
+    return isAuthenticated ? <Navigate to='/chat'/> : children
+  }
+
+
   return (
     <div>
       <Routes>
-        <Route path='/auth' element={<Auth/>}></Route>
-        <Route path='/chat' element={<Chat/>}></Route>
-        <Route path='/profile' element={<Profile/>}></Route>
+        <Route path='/auth' element={
+          <AuthRoute>
+            <Auth/>
+          </AuthRoute>}>
+        </Route>
+
+        <Route path='/chat' element={
+          <PrivateRoute>
+            <Chat/>
+          </PrivateRoute>
+        }></Route>
+        
+        <Route path='/profile' element=
+          { 
+            <PrivateRoute>
+             <Profile/>
+            </PrivateRoute>
+          }
+        ></Route>
+        
         <Route path='*' element={<Navigate to= '/auth'/>}></Route>
       </Routes>
     </div>
