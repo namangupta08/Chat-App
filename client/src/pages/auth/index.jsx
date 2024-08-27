858422f4dabda8c10dedc5e94c0c10d3661abba5
namangupta08 <guptanaman032@gmail.com>
2024-08-27 22:44:03 +0530

    auth index updation

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -7,11 +7,15 @@ import { Input } from "@/components/ui/input"
 import { toast } from 'sonner'
 import { apiClient } from '@/lib/api-client'
 import { SIGNUP_ROUTE , LOGIN_ROUTE } from '@/utils/constants'
+import { useNavigate } from 'react-router-dom'
+import { useAppStore } from '@/store'
 
 
 
 function Auth() {
 
+  const navigate = useNavigate();
+  const {setUserInfo} = useAppStore();
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [confirmPassword, setconfirmPassword] = useState("")
@@ -54,6 +58,15 @@ function Auth() {
   const handleLogin = async () => {
     if(validateLogin()){
       const response = await apiClient.post(LOGIN_ROUTE , {email , password} , {withCredentials:true})
+      if(response.data.user.id){
+        setUserInfo(response.data.user);
+        if(response.data.user.profileSetup){
+          navigate('/chat')
+        }
+        else{
+          navigate('/profile');
+        }
+      }
       console.log(response)
     }
   }
@@ -61,6 +74,10 @@ function Auth() {
   const handleSignup = async () => {
     if(validateSignUp()){
      const response = await apiClient.post(SIGNUP_ROUTE , {email , password} , {withCredentials:true})
+     if(response.status == 201){
+      setUserInfo(response.data.user);
+      navigate('/profile')
+     }
      console.log(response)
     }
   
@@ -85,7 +102,7 @@ function Auth() {
             </div>
 
             <div className='flex items-center justify-center w-full'>
-             <Tabs className='w-3/4'>
+             <Tabs className='w-3/4' defaultValue='login'>
                 <TabsList className='bg-transparent w-full rounded-none'>
                   <TabsTrigger value="login"
                   className='data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300'
