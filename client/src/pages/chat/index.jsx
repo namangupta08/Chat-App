7a1c4509e5bacc8b2333d593a6df4fbdf4f27e73
namangupta08 <guptanaman032@gmail.com>
2024-08-27 23:55:08 +0530

    index updated

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { useAppStore } from '@/store'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 function Chat() {
+  
+  const {userInfo} = useAppStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!userInfo.profileSetup){
+      toast('Please set up profile to continue')
+      navigate('/profile')
+    }
+  },[userInfo , navigate])
   return (
     <div>
       chat
