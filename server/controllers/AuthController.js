a18fc2584585fe23ded276afc0101ae921a6c095
namangupta08 <guptanaman032@gmail.com>
2024-08-27 22:44:31 +0530

    controller updtated

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,3 +1,4 @@
+import { compare } from "bcrypt";
 import User from "../models/UserModel.js";
 import jwt  from "jsonwebtoken";
 
@@ -31,15 +32,88 @@ export const SignUp = async (req,res , next) => {
             user:{
                 id:user.id,
                 email:user.email,
-                // firstName:user.firstName,
-                // lastName:user.lastName,
-                // image:user.image,
                 profileSetup:user.profileSetup
             }
         })
 
     } 
     catch (error) {
+        console.log(error);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+export const Login = async (req,res,next) => {
+    try {
+        const {email , password} = req.body;
+        if(!email || !password){
+            return res.status(400).send({
+                success: false,
+                message: "Email and password are required",
+            })
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(404).send({
+                success: false,
+                message: "User with the given email not found",
+            })
+        }
+
+        const auth = await compare(password , user.password)
+
+        if(!auth){
+            return res.status(400).send({
+                success: false,
+                message: "Password is incorrect",
+            })
+        }
+
+        //jwt token
+
+        res.cookie("JWT" , createToken(email , user.id) , {
+            maxAge,
+            secure:true,
+            sameSite:"None"
+        });
+
+        return res.status(200).json({
+            user:{
+                id:user.id,
+                email:user.email,
+                profileSetup: user.profileSetup,
+                firstName:user.firstName,
+                lastName:user.lastName,
+                image:user.image,
+                color:user.color,
+                
+            }
+        })
+
+    } 
+    catch (error) {
+        console.log(error);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+export const getUserInfo = async (req,res,next) => {
+    try {
+        // return res.status(200).json({
+        //     user:{
+        //         id:user.id,
+        //         email:user.email,
+        //         profileSetup: user.profileSetup,
+        //         firstName:user.firstName,
+        //         lastName:user.lastName,
+        //         image:user.image,
+        //         color:user.color,
+                
+        //     }
+        // })
+    } catch (error) {
         console.log(error);
         return res.status(500).send("Internal Server Error");
     }
