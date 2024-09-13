a106651402f5a1b527193b99c224ae36f2faf762
namangupta08 <guptanaman032@gmail.com>
2024-09-13 19:54:58 +0530

    v2 updation

--- client/src/lib/api-client.js
+++
@@ -1,9 +0,0 @@
-//const { HOST } = require("@/utils/constants");
-//const { default: axios } = require("axios");
-
-import axios from "axios";
-import { HOST } from "@/utils/constants.js";
-
-export const apiClient = axios.create({
-  baseURL: HOST,
-});
--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++
@@ -1,9 +0,0 @@
-function MessageConatiner() {
-  return (
-    <div className="flex h-[80vh] overflow-y-auto scrollbar-hidden py-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full">
-      naman
-    </div>
-  );
-}
-
-export default MessageConatiner;
