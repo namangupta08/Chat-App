5d82a5ce35a5b261c83b5cfb60b4f80bfe9d6ccf
namangupta08 <guptanaman032@gmail.com>
2024-10-24 00:42:27 +0530

    updation

--- server/index.js
+++ server/index.js
@@ -21,10 +21,12 @@ app.use(cors({
     credentials:true,
 }))
 
+app.use(cookieParser())
+
 app.use("/uploads/profiles" , express.static("uploads/profiles"))
 app.use("/uploads/files" , express.static('uploads/files') )
 
-app.use(cookieParser())
+
 app.use(express.json())
 
 app.use("/api/auth" , AuthRoutes)
