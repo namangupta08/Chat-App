2eda1a6acce2ffd322173bf9c21df102ff4f8d11
namangupta08 <guptanaman032@gmail.com>
2024-10-23 20:28:50 +0530

    updation

--- server/models/MessagesModel.js
+++ server/models/MessagesModel.js
@@ -1,40 +1,39 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const MessageSchema = new mongoose.Schema({
-    sender:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Users",
-        required:true,
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: false,
+  },
+  messageType: {
+    type: String,
+    enum: ["text", "file"],
+    required: true,
+  },
+  content: {
+    type: String,
+    required: function () {
+      return this.messageType === "text";
     },
-    recipient:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Users",
-        required:false,
+  },
+  fileUrl: {
+    type: String,
+    required: function () {
+      return this.messageType === "file";
     },
-    messageType:{
-        type:String,
-        enum:["text" , "file"],
-        required:true,
-    },
-    content:{
-        type:String,
-        required: function(){
-            return this.messageType === "text"
-        },
-    },
-    fileUrl:{
-        type:String,
-        required: function(){
-            return this.messageType === "file"
-        },
-    },
-    timeStamp:{
-        type:Date,
-        default:Date.now,
-    }
+  },
+  timeStamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-})
-
-const Message = mongoose.model("Messages" , MessageSchema)
+const Message = mongoose.model("Messages", MessageSchema);
 
 export default Message;
